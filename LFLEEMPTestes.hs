e7a352d08ed9ca7e0f5a7f919aae90b461d65469
Luisa Sinzker Fantin <luisasf1408@gmail.com>
2017-05-06 17:28:28 -0300

    Add files via upload

---
+++ LFLEEMPTestes.hs
@@ -0,0 +1,36 @@
+module LFLEEMPTestes where
+
+import LFLE01EMP
+
+import Test.HUnit
+
+add = DecFuncao "add" ["x","y"] ((Soma (Ref "z"))(Soma (Ref "x")(Ref "y")))
+sub = DecFuncao "sub" ["x","y"] ((Subtracao (Ref "z"))(Subtracao (Ref "x")(Ref "y")))
+mult = DecFuncao "mult" ["x","y"] ((Multiplicacao (Ref "z"))(Multiplicacao (Ref "x")(Ref "y")))
+
+amb1 = [add]
+amb2 = [sub]
+amb3 = [mult]
+
+apAdd = Aplicacao "add" [(Valor 1), (Valor 2)]
+apSub = Aplicacao "sub" [(Valor 1), (Valor 2)]
+apMul = Aplicacao "mult" [(Valor 1), (Valor 2)]
+
+let01 = Let "z" (Valor 4)(apAdd)
+--      Let "z" (Valor 4)(Aplicacao "apAdd" [(Valor 1), (Valor2)])
+let02 = Let "z" (Valor 4)(apSub)
+let03 = Let "z" (Valor 4)(apMul)
+
+avAdd = avaliar let01 amb1
+--      avaliar (Let "z" (Valor 4)(Aplicacao "apAdd" [(Valor 1), (Valor 2)])) (DecFuncao "add" ["x","y"] (Soma (Ref "z")(Soma (Ref "x")(Ref "y"))))
+--  let z = 4
+--    in let add x y = z+x+y
+--         in add 2 3
+-- >9
+
+avSub = avaliar let02 amb2
+-- let z = 4
+--    in let sub x y = z-(y-x)
+--          in sub 2 1
+-- >5
+avMul = avaliar let03 amb3
